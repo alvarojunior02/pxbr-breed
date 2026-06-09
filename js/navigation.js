ccb5859ba312f296de085d769dd6cf0f9e628345
Alvaro Junior <alvarojunior02.dev@gmail.com>
2026-06-09 00:17:50 -0400

    refactor: add app layout and section navigation

---
+++ js/navigation.js
@@ -0,0 +1,29 @@
+const navLinks = document.querySelectorAll(".nav-link");
+
+const appSections = document.querySelectorAll(".app-section");
+
+function showSection(sectionId) {
+    appSections.forEach(section => {
+        section.classList.remove("active-section");
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+    });
+
+    document
+        .getElementById(sectionId)
+        .classList
+        .add("active-section");
+
+    document
+        .querySelector(`[data-section="${sectionId}"]`)
+        .classList
+        .add("active");
+}
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        showSection(link.dataset.section);
+    });
+});
